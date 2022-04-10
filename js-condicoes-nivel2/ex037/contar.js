0f6fa5bed7d06336872e9908e1510fe5daee26f4
jonasjps <jonaspsjw@hotmail.com>
2022-04-10 20:09:14 -0300

    Atualizando

---
+++ js-condicoes-nivel2/ex037/contar.js
@@ -0,0 +1,9 @@
+ function contar() {
+     let res = document.querySelector('div#result')
+     res.innerHTML += '<h2>Contagem regressiva de 10 a 1</h2>'
+    for(let c = 10 ; c >= 1 ; c--) {
+        res.innerHTML += ` ${c} &#x1f449; `
+        
+    }
+    res.innerHTML += '&#x1f3c1;'
+ }
