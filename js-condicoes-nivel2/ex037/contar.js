bad79db53e3c69cd6dcf75b8ad5ae29a1108210b
jonasjps <jonaspsjw@hotmail.com>
2022-04-10 20:10:12 -0300

    ex 037 repetições js

--- js-condicoes-nivel2/ex037/contar.js
+++ js-condicoes-nivel2/ex037/contar.js
@@ -2,7 +2,7 @@
      let res = document.querySelector('div#result')
      res.innerHTML += '<h2>Contagem regressiva de 10 a 1</h2>'
     for(let c = 10 ; c >= 1 ; c--) {
-        res.innerHTML += ` ${c} &#x1f449; `
+        res.innerHTML += ` <strong>${c}</strong> &#x1f449; `
         
     }
     res.innerHTML += '&#x1f3c1;'
