95f8ab701b0aa5cfe166f939bb86c409c564d3db
jonasjps <jonaspsjw@hotmail.com>
2022-04-11 21:41:15 -0300

    praticando

--- js-condicoes-nivel2/ex041/praticando.js
+++ js-condicoes-nivel2/ex041/praticando.js
@@ -1,8 +1,8 @@
-var c = n
-    var fat = 1
-    while(c > 1) {
-        console.log (`${c} x `) ;
-        fat *= c
-        c--
-    }
-    console.log  (`1 =  ${fat.toLocaleString('pt-BR')}`);
+var c = 4
+var fat = 1
+while(c > 1) {
+    console.log (`${c} x `) 
+    fat *= c
+    c--
+}
+console.log  (`1 =  ${fat.toLocaleString('pt-BR')}`)
